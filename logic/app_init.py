f8893fe9cfcedd0d48c12c04cb7cb40dd070215b
Ksenniya <lukkonina1@gmail.com>
2024-12-22 18:10:15 -0300

    quart-client-template

---
+++ logic/app_init.py
@@ -0,0 +1,9 @@
+from common.ai.ai_assistant_service import AiAssistantService
+from common.auth.auth import authenticate
+from common.repository.cyoda.cyoda_repository import CyodaRepository
+from common.service.service import EntityServiceImpl
+
+cyoda_token = authenticate()
+ai_service = AiAssistantService()
+entity_repository = CyodaRepository()
+entity_service = EntityServiceImpl()
